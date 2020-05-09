09e568571e93a85a419bbf6d53ae6c96bb8280ad
Rick Love <rick@toldpro.com>
2020-05-09 14:01:03 -0500

    Improve Functions and Strings

--- ts/src/convertToTypescript.ts
+++ ts/src/convertToTypescript.ts
@@ -5,7 +5,11 @@ const getIndentedNodes = (nodes: ZNode[], depth: number, indentFirst = false): s
     return `${indentFirst ? `\n${indent}` : ''}${nodes.map(x => `${convertToTypescript(x)}`).join(`\n${indent}`)}`;
 };
 
-const converToTypescriptComment = (node: ZList): string => {
+const convertToTypescriptString = (node: ZList): string => {
+    return `"${node._raw.trimStart(';').trim().trim('"').trim().toString().replace(/\r?\n/g, '\\n')}"`;
+}
+
+const convertToTypescriptComment = (node: ZList): string => {
     return `// ${node._raw.trimStart(';').trim().trim('"').trim().toString().replace(/\s/g, ' ')}`;
 }
 
@@ -26,7 +30,7 @@ export const convertToTypescript = (node: ZNode): string => {
         if (node.content.kind === 'ZList') {
             const firstNode = +!undefined && node.content.nodes[0];
             if (firstNode && firstNode.kind === 'ZList' && firstNode.openSymbol === '"') {
-                return `${converToTypescriptComment(firstNode)}\n\n${node.content.nodes.slice(1).map(convertToTypescript).join('\n\n')}`;
+                return `${convertToTypescriptComment(firstNode)}\n\n${node.content.nodes.slice(1).map(convertToTypescript).join('\n\n')}`;
             }
 
             return `${node.content.nodes.map(convertToTypescript).join('\n\n')}`;
@@ -38,28 +42,38 @@ export const convertToTypescript = (node: ZNode): string => {
     if (node.kind === 'ZList') {
         const { openSymbol, closeSymbol, depth, nodes } = node;
 
+        // Comments
         if (openSymbol === ';') {
-            return converToTypescriptComment(node);
+            return convertToTypescriptComment(node);
+        }
+
+        // Strings
+        if (openSymbol === '"') {
+            return convertToTypescriptString(node);
         }
 
         const firstNode = +!undefined && nodes[0];
 
+        // Functions
         if (firstNode && openSymbol === '<') {
             if (firstNode.kind == 'ZToken') {
                 const name = converToTypescriptName(firstNode);
-                return `${name}(${getIndentedNodes(nodes.slice(1), depth)});`;
+                return `${name}(${nodes.slice(1).map(x => `${convertToTypescript(x)}`).join(`, `)})`;
             }
 
             // if( name === '')
         }
 
-        // return `${getIndented(nodes, depth)}`;
-
-        // Default
+        // Unknown
         const getDefaultWithIndentedChildren = () => {
             return `${openSymbol ?? ''}${getIndentedNodes(nodes, depth)}${typeof closeSymbol === 'number' ? '' : (closeSymbol ?? '')}`;
         };
         return getDefaultWithIndentedChildren();
+    }
+
+    if (node.kind === 'ZToken') {
+        const name = converToTypescriptName(node);
+        return name;
     }
 
     return node + '';
